528572600b9a3dfcd090f89963cb1b3077e25c63
Tanya <Sunnytanya93@mail.ru>
2022-07-19 17:50:23 +0700

    Part_7 The last step in the project is to ensure the security of our service. By now, all data, authentication, and business information is transmitted over an unsecured communication channel and can be easily intercepted. The solution to this problem is the HTTPS protocol. Implementing HTTPS in Spring Boot is easy, but first, you need to generate a private key and a certificate. To do this, you can use the keytool utility that is included in the JDK. Save the certificate file in the keystore directory in the resource folder of the Spring Boot application:
        keytool -genkeypair -alias accountant_service -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore keystore.p12 -validity 3650
    
        [ALERT]In this example, we use a self-signed certificate, which is acceptable for a training project, but in real systems it is required to use a certificate signed by a trusted certification authority.
    
        Next, make changes to the application properties:
        server.ssl.enabled=true
        # Keystore format
        server.ssl.key-store-type=PKCS12
        # The path to the keystore
        server.ssl.key-store=classpath:keystore/service.p12
        # Certificate password
        server.ssl.key-store-password=service
        # Certificate alias
        server.ssl.key-alias=accountant_service
    
       Objectives
       Generate the certificate with CN=accountant_service;
       Add the HTTPS support using the previously generated certificate. The tests will check only the CN attribute of the certificate.

--- src/main/java/com/example/Account_Service/AuthenticationFailureException.java
+++
@@ -1,9 +0,0 @@
-//package com.example.Account_Service;
-//
-//import org.springframework.security.core.AuthenticationException;
-//
-//public class AuthenticationFailureException extends AuthenticationException {
-//    public AuthenticationFailureException(String message) {
-//        super(message);
-//    }
-//}
